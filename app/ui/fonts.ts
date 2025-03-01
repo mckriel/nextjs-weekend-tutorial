dc4b1564df3e0798eed856cda24031e75f87c8e1
mckriel <mckriel@gmail.com>
2025-03-01 12:41:02 +0200

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
+const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+
+export {
+    inter,
+    lusitana,
+}
